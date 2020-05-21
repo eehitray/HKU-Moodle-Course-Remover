3a920e9040855d9c9f5f2157412ba960a3704504
Eehit Ray <greatblitz982@gmail.com>
2020-05-21 21:52:06 +0800

    Remove links from sidebar

--- content.js
+++ content.js
@@ -1,12 +1,24 @@
 chrome.storage.local.get("courses", function(items) {
-	var divTags = document.querySelectorAll("div[data-courseid]");
 	var searchTexts = items["courses"].split("\n");
 
-	for (var i = 0; i < divTags.length; i++) {
-		for (var j = 0; j < searchTexts.length; j++) {
-			if (divTags[i].innerHTML.includes(searchTexts[j])) {
-				divTags[i].remove();
-				break;
+	if (!(searchTexts.length == 1 && searchTexts[0] == '')) {
+		var divTagsMain = document.querySelectorAll("div[data-courseid]");
+		for (var i = 0; i < divTagsMain.length; i++) {
+			for (var j = 0; j < searchTexts.length; j++) {
+				if (divTagsMain[i].innerHTML.includes(searchTexts[j])) {
+					divTagsMain[i].remove();
+					break;
+				}
+			}
+		}
+
+		var divTagsSide = document.querySelectorAll("div[class='column c1']");
+		for (var i = 0; i < divTagsSide.length; i++) {
+			for (var j = 0; j < searchTexts.length; j++) {
+				if (divTagsSide[i].innerHTML.includes(searchTexts[j])) {
+					divTagsSide[i].remove();
+					break;
+				}
 			}
 		}
 	}
